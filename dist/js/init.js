1a9ae40043d07c1218c09a15c5dc4c61221f5d30
ddmee <mee.donal@gmail.com>
2016-03-09 18:06:14 +0000

    update eventhanlder and html, no form in reply button

--- dist/js/init.js
+++ dist/js/init.js
@@ -1,10 +1,25 @@
-// Init JS
-
 function init() {
-		$("#q_id, #q_submit, #rep_submit").change(function(event) {
-		console.log(event);
+
+		$("#q_submit").click(function(event) {
+		//console.log(event);
+
 		// Get data form question input element. 
 		var entry = $("#q_id").val();
+		console.log(entry);
+		});
+
+		$("#rep_submit").click(function(event) {
+		//console.log(event);
+
+		var li_tag = $(event.target).parent();
+		var unique_id = $(li_tag).find("#unique_id");
+		console.log(unique_id);
+		console.log(unique_id.html());
+		// Get data form question input element. 
+		//event.parent
+		// var question_id = $("#unique_id").val();
+		// console.log('question id is:' + question_id);
+		
 		});
 }
 $(init);
