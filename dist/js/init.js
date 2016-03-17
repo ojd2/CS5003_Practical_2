c38605e40f769490f53d3168548fa17681ffb5e7
ojd2 <ojd2@st-andrews.ac.uk>
2016-03-17 17:19:13 +0000

    Updated init.js file. Introduced new logic in order to list all questions, empty and re-load after new question is submitted. Edited and tidied up elsewhere. Now need to focus on replies.

--- dist/js/init.js
+++ dist/js/init.js
@@ -1,66 +1,52 @@
 // ----------------------------------------------------------------------
 // ----------------------------------------------------------------------
-/*
-*	First, begin with some global data vars.
-*/ 
+//	First, begin with some global data vars.
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
 var response = [];
-var data, user;
-/*
-*	Next, begin with some global HTML vars.
-*/
+var req, data, user;
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+//	Next, begin with some global HTML vars.
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
 var container, question, inner_q, panel_q, header_q, inner_panel, rep_q,
 rep_text, rep_submit, rep_reply, q_title, q_meta;
 
 // ----------------------------------------------------------------------
 // ----------------------------------------------------------------------
-/*
-* Function to retrieve response & appended question to HTML question.
-*/
+// Function to retrieve response & appended question to HTML question.
 // ----------------------------------------------------------------------
 // ----------------------------------------------------------------------
 function displayQuestion(objects) {
-	// Simple for loop to iterate over our returned objects.
-	// for(var i in objects) {	
-	// 	// Store our question inside the JSON into data.
-	// 	data = objects[i].question;
-	// 	user = objects[i].user;
-	// }
-	// // Call our container HTML method.
-	// Q_Container();
-	// // Call our inner panel & header HTML method.
-	// Q_Panel();
-	// // Call our replies HTML method.
-	// Q_Replies();
-	
+	// Clear empty.
+    $('.questions').html('');
+	// Call sort method once again for parsed response.
+	orderKeys(objects);
 	// Simple debugging message.
 	console.log("Done");
 }
 // ----------------------------------------------------------------------
 // ----------------------------------------------------------------------
-/*
-* Function to retrieve response & appended all questions to HTML question.
-*/
+// Function to retrieve response & appended all questions to HTML question.
 // ----------------------------------------------------------------------
 // ----------------------------------------------------------------------
 function displayAll(objects) {
-	var req = new XMLHttpRequest();
+	req = new XMLHttpRequest();
     req.open("GET", "questions");
     req.setRequestHeader("Content-Type", "text/plain");
     req.onreadystatechange = function() {
-        // Call our loop through method.
-        loopArray(JSON.parse(req.responseText));
-		// Loop through.
-      	function loopArray(array) {
-      		console.log('inside loopArray');
 
+        // Call our loopArray method below.
+        loopArray(objects);
+
+		// Loop through.
+      	function loopArray() {
       		for(var i in objects) {	
       			// Store our question inside the JSON into data.
-				data = objects[i].question;
-				user = objects[i].user;
-
 				// Create our <li> '.question_container' to append to <ul> 'questions'
-				var container = document.getElementsByClassName('questions');
-				var question = document.createElement( "li" );
+				container = document.getElementsByClassName('questions');
+				question = document.createElement( "li" );
 			  	question.className = 'question_container';
 			  	question.innerHTML = '<div class="question_panel">' + 
 			  	'<div class="question_heading">' + 
@@ -101,31 +87,45 @@ function displayAll(objects) {
 }
 // ----------------------------------------------------------------------
 // ----------------------------------------------------------------------
-/*
-* Retrieve ALL data by making an AJAX request.
-*/
+// Retrieve ALL data then apply sort algorithm.
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+function orderKeys(objects) {
+var sorted = [];
+ Object.keys(objects)
+    .sort(function(a, b) {
+  			return a - b;
+	}).reverse()
+	
+    .forEach(function(v, i) {
+          console.log(objects[v]);
+          sorted.push(objects[v]);         
+   	});
+   	// Call to our display method.
+   	displayAll(sorted);
+}
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+// Retrieve ALL data by making an AJAX request.
 // ----------------------------------------------------------------------
 // ----------------------------------------------------------------------
 function getResponse() {
-    var req = new XMLHttpRequest();
+    req = new XMLHttpRequest();
     req.open("GET", "questions");
     req.setRequestHeader("Content-Type", "text/plain");
     req.onreadystatechange = function() {
-  		// Call displayQuestion whilst parsing our objects.
-        displayAll(JSON.parse(req.responseText));
+        // Call sorting method for our parsed response.
+    	orderKeys(JSON.parse(req.responseText));
     }
     req.send(null);
 }
-
 // ----------------------------------------------------------------------
 // ----------------------------------------------------------------------
-/*
-* Retrieve the question list by making an AJAX request.
-*/
+// Retrieve the question list by making an AJAX request.
 // ----------------------------------------------------------------------
 // ----------------------------------------------------------------------
-function getQuestions() {
-    var req = new XMLHttpRequest();
+function getQuestion() {
+    req = new XMLHttpRequest();
     req.open("GET", "questions");
     req.setRequestHeader("Content-Type", "text/plain");
     req.onreadystatechange = function() {
@@ -136,12 +136,12 @@ function getQuestions() {
 }
 // ----------------------------------------------------------------------
 // ----------------------------------------------------------------------
-/*
-* Add a new question by making POST request to node server. Not finished.
-* Uses dumbie values for the mean-time.
-**/
+// Add a reply by making POST request to node server. Not finished.
+// Uses dummy values for the mean-time.
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
 function sendReply(){
-    var req = new XMLHttpRequest();
+    req = new XMLHttpRequest();
     req.open("POST", "reply");
     req.setRequestHeader("Content-Type", "text/plain");
     req.send('{"q_id":"2","reply":"We have successfull sent a reply to quesiton 2"}');
@@ -150,20 +150,19 @@ function sendReply(){
      	//getQuestions();   	
     }
 }
-
-/*
-* Add a new question by making POST request to node server
-* question = a new question from user.
-*/
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+// Add a new question by making POST request to node server
+// question = a new question from user.
 // ----------------------------------------------------------------------
 // ----------------------------------------------------------------------
 function sendQuestion(question){
-    var req = new XMLHttpRequest();
+    req = new XMLHttpRequest();
     req.open("POST", "questions");
     req.setRequestHeader("Content-Type", "text/plain");
     req.onreadystatechange = function() {
         //console.log(req.responseText);
-     	getQuestions();
+     	getQuestion();
     }
     req.send(question);
 
@@ -171,15 +170,12 @@ function sendQuestion(question){
 }
 // ----------------------------------------------------------------------
 // ----------------------------------------------------------------------
-/*
-* Initilise function to start application onload.
-*/
+// Initilise function to start application onload.
 // ----------------------------------------------------------------------
 // ----------------------------------------------------------------------
 function init() {
 	// Display all questions on page load.
 	getResponse();
-
 
 	// Event handler for new question submission.
 	$("#q_submit").click(function(event) {
